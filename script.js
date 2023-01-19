81fa36c68164d843400ffe71c7111e7901873e11
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-01-19 20:00:06 -0300

    🚀 Aula 4

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+const localStorageKey = 'NLWSetup@habits'
 
-const data = {
-  run: ['01-18',],
-  water: ['01-15', '01-16', '01-17', '01-18', '01-19'],
-  study: ['01-15', '01-16', '01-17', '01-18', '01-19'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  // const today = '19/01'
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('⚠️ Dia já incluso')
+    return
+  }
+  alert('✅ Adicionado com sucesso')
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem(localStorageKey, JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem(localStorageKey)) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
